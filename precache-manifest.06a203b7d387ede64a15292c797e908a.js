2ab0075cbd6f7e40af125f575741f0db71cbf6a3
unknown <fmreda3@gmail.com>
2020-05-31 03:46:46 +0100

    Updates

---
+++ precache-manifest.06a203b7d387ede64a15292c797e908a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28d74befee8b2e597ad6f1eb22b7081",
+    "url": "/Klir-twitter-devs/index.html"
+  },
+  {
+    "revision": "24de1fa4b3ff48de4afb",
+    "url": "/Klir-twitter-devs/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c03513c9b78b1f1a4d79",
+    "url": "/Klir-twitter-devs/static/css/main.ad6a27b1.chunk.css"
+  },
+  {
+    "revision": "24de1fa4b3ff48de4afb",
+    "url": "/Klir-twitter-devs/static/js/2.8abd1b08.chunk.js"
+  },
+  {
+    "revision": "a8708c1560a44a3a2f75343bf0ed33a1",
+    "url": "/Klir-twitter-devs/static/js/2.8abd1b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03513c9b78b1f1a4d79",
+    "url": "/Klir-twitter-devs/static/js/main.8850a8d7.chunk.js"
+  },
+  {
+    "revision": "3828be7752b822116e2a",
+    "url": "/Klir-twitter-devs/static/js/runtime-main.294bb68f.js"
+  }
+]);
