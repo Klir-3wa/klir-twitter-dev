bb3fc7fe6887f38e2df213b7c59c4bae9cd81d40
lahcen001 <lahcen0000lh@gmail.com>
2020-06-03 22:25:26 +0200

    bootstrap theme

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton,
+} from "react-twitter-embed";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="container">
         <h1>Home Page</h1>
+        <TwitterMomentShare
+    momentId={'650667182356082688'}
+  />
+
       </div>
     );
   }
