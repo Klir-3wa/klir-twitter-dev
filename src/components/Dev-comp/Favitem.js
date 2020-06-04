c1584e3c1d1db1aa40104db6b714922077b59ad5
lahcen001 <lahcen0000lh@gmail.com>
2020-06-04 20:57:09 +0200

    btn delete

---
+++ src/components/Dev-comp/Favitem.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterFollowButton,
+    TwitterHashtagButton,
+    TwitterMentionButton,
+    TwitterTweetEmbed,
+    TwitterMomentShare,
+    TwitterDMButton,
+    TwitterVideoEmbed,
+    TwitterOnAirButton,
+  } from "react-twitter-embed";
+export class Favitem extends Component {
+
+delete=(id)=>{
+this.props.delete1(id);
+// window.location.reload();
+}
+
+  render() {
+    return (
+    
+
+
+         <div className="col-md-6 mt-4" key={this.props.tw.twitterID}>
+                <TwitterTweetEmbed tweetId={this.props.tw.twitterID}/>
+                <button onClick={()=>this.delete(this.props.tw.id )}>Supprimer</button>
+              </div>
+              
+
+
+      
+    )
+  }
+}
+
+export default Favitem
--- src/components/Dev-comp/NewComp.js
+++ src/components/Dev-comp/NewComp.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Favitem from './Favitem';
 import {
   TwitterTimelineEmbed,
   TwitterShareButton,
@@ -33,49 +34,42 @@ getdata(e){
          
       })
   })
+  
 }
 componentDidMount(){
   this.getdata();
  
 }
 
-delete(id){
+delete2(id){
   
-
-
-  console.log(id.id);
-  axios.delete(`http://127.0.0.1:8000/api/delete/${id.id}`)
+  console.log(id);
+  axios.delete(`http://127.0.0.1:8000/api/delete/${id}`)
   .then((res)=>{
- console.log(res)
-
+    console.log(res)
+    window.location.reload();
+ 
   })
-
  
-  // let n = twt.findIndex(twt=>twt.id == id.id)
-  //    twt.splice(n,1)
-  //    console.log(twt)
-  //    this.setState({
-  //      tweet2 :twt
-  //    })
- 
-
 }
-
   render() {
     return (
       <div onSubmit={this.getdata}>
-        <h1>new Compoenent</h1>
+        <h1>Les favoris Tweets</h1>
 
         <div className="container" >
           <div className="row mt-5">
             {this.state.tweet2.map((tw) => (
-              <div className="col-md-4 mt-4" key={tw.twitterID}>
-                <TwitterTweetEmbed tweetId={tw.twitterID}/>
-                <button onClick={this.delete.bind(null, { id: tw.id })}>Supprimer</button>
-              </div>
-              
+              // <div className="col-md-4 mt-4" key={tw.twitterID}>
+              //   <TwitterTweetEmbed tweetId={tw.twitterID}/>
+
+              //   <button onClick={this.delete.bind(null, { id: tw.id })}>Supprimer</button>
+              // </div>
+             
+       <Favitem   tw={tw}   delete1={this.delete2}/>
             ))
             }
+
 
          
           </div>
